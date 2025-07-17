b40f1f6ef62b5a682f512dc4365a6298f4d30b38
ejshafran <ejshafran@hilma.tech>
2025-07-17 12:44:44 +0300

    test: add super basic test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,15 @@
+import { describe, expect, it } from "vitest";
+
 import dayjs from "dayjs";
 import timeInterval from "./index";
 
 dayjs.extend(timeInterval);
+
+describe("dayjs.timeInterval", () => {
+  it("should create TimeInterval objects", () => {
+    const timeInterval = dayjs.timeInterval({ start: dayjs(), end: dayjs() });
+    expect(dayjs.isTimeInterval(timeInterval)).toBe(true);
+  });
+});
 
 // TODO: add tests here
